f34280fa60698fab263d3673c95ee6e7141c67b4
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-03-19 15:24:20 -0400

    render views

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const projects = require('../projects.json');
 
 // colocar las rutas aquí
 router.get('/', (req, res) => {
-    res.send("Welcome to express");
+    return res.render('home');
 });
 
+router.get('/services', (req, res) => {
+    res.render('services');
+});
+
+router.get('/aboutme', (req, res) => {
+    return res.render('aboutme');
+});
+
+router.get('/projects/', (req, res) => {
+    return res.render('projects', { projects });
+});
 
 module.exports = router;
